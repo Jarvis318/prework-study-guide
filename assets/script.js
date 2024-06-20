3113033d98cc387cce397c2a8c447105454ce222
Jarvis <jarvwashington@gmail.com>
2024-06-19 21:21:07 -0500

    <Added Javascript code to prework project>

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//<script src="./assets/script.js"></script> Make sure this is correct,
+//and at the bottom of the index file below the footer and above the body.
+
+var topics = ['HTML', 'CSS', 'Git','JavaScript'];
+var x = 0;
+
+for(var x = 0; x < topics.length; x++)
+  {
+    console.log(topics[x]);
+  }
+
+  /*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
